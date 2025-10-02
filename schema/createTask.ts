96ac93e358c7ffdd16909df8e23fdbd2a2bcc065
success9381-creator <success9381@gmail.com>
2025-10-02 18:45:27 +0530

    Add files via upload

---
+++ schema/createTask.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const createTaskSchema = z.object({ 
+    collectionId: z.number().nonnegative(),
+    content: z.string().min(8, { message: "Task must be at least 8 characters long" }),
+    expiresAt: z.date().optional()
+})
+
+export type createTaskSchemaType = z.infer<typeof createTaskSchema>
